165558661343c083a25060559e997da63b27dc02
badaozhai <89066216@qq.com>
2016-07-26 20:11:29 +0800

    注释div

--- controllers/jsondiff.go
+++ controllers/jsondiff.go
@@ -1,31 +1,28 @@
 package controllers
 
 import (
-	"github.com/astaxie/beego"
 	"blog/tool"
 	"fmt"
+	"github.com/astaxie/beego"
 )
 
-type JsonDiffController struct  {
+type JsonDiffController struct {
 	beego.Controller
 }
-func (c *JsonDiffController) Get(){
-	aurl:=c.Input().Get("aurl")
-	burl:=c.Input().Get("burl")
+
+func (c *JsonDiffController) Get() {
+	aurl := c.Input().Get("aurl")
+	burl := c.Input().Get("burl")
 	var ajson string
 	var bjson string
-	if aurl!="" && burl !=""{
+	if aurl != "" && burl != "" {
 		ajson = tool.GetHtml(aurl)
 		bjson = tool.GetHtml(burl)
 	}
-	c.Data["Aurl"] = aurl;
-	c.Data["Burl"] = burl;
+	c.Data["Aurl"] = aurl
+	c.Data["Burl"] = burl
 	c.Data["Ajson"] = ajson
 	c.Data["Bjson"] = bjson
 	fmt.Println(ajson)
-	c.TplName = "json-diff.html";
-}
-
-func getHtml(url string) string {
-	return ""
+	c.TplName = "json-diff.html"
 }
