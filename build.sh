5e0d0fd64c6a2b9bb04ea56932177f9159343ea5
badaozhai <89066216@qq.com>
2016-09-12 22:17:49 +0800

    google

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+GOOS=linux GOARCH=amd go build -o $GOPATH/bin/blogbin/blog
--- controllers/default.go
+++ controllers/default.go
@@ -2,6 +2,10 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+
+	"go-executor/util/http_client"
+	"go-executor/request"
+	"fmt"
 )
 
 type MainController struct {
@@ -9,7 +13,43 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "goer.pub"
-	c.Data["Email"] = "goudanbaba123@gmail.com"
-	c.TplName = "index.html"
+	session := request.NewSession()
+	URL := `https://www.google.com`
+	html,err:= http_get(session,URL)
+	if err != nil{
+		fmt.Println(err)
+		c.Ctx.WriteString("hello,world")
+	}else{
+		c.Ctx.WriteString(html)
+	}
+
+}
+
+func http_get(session *request.RequestSession,URL string)(string,error){
+	httpclient := http_client.New(URL, nil)
+	req := httpclient.Req.Header
+	req.Add("Host", " www.google.com")
+	req.Add("Connection", " keep-alive")
+	req.Add("Cache-Control", " no-cache")
+	req.Add("Accept", " */*")
+	//req.Add("X-Requested-With", " XMLHttpRequest")
+	//req.Add("If-Modified-Since", " 0")
+	req.Add("User-Agent", " " + session.UserAgent)
+	//req.Add("Referer", " https://kyfw.12306.cn/otn/leftTicket/init")
+	req.Add("Accept-Encoding", " gzip, deflate, sdch, br")
+	req.Add("Accept-Language", " zh-CN,zh;q=0.8,en;q=0.6")
+	if session.Cookie.String() != "" {
+		req.Add("Cookie", session.Cookie.String())
+	}
+	res, err := httpclient.Do()
+	if err != nil {
+		return "", err
+	}
+	// 更新cookie
+	session.Cookie.Update(res)
+	html, err := http_client.ReadResponse(res)
+	if err != nil {
+		return "", err
+	}
+	return html,nil
 }
